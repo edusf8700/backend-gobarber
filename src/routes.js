8b9099eeb384c652a73b0007097481b3257307cb
Eduardo Freitas <edusf8700@gmail.com>
2019-10-31 04:57:28 -0200

    criado model de usuário e criado o loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Edu' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Edu',
+    email: 'edusf8700@gmail.com',
+    password_hash: '123456',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
